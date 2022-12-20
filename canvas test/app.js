557a4dae52147332ea77877df2a6bd2f0fc8258e
sycrw <114520804+sycrw@users.noreply.github.com>
2022-12-20 05:44:14 -0600

    Add files via upload

---
+++ canvas test/app.js
@@ -0,0 +1,34 @@
+function setup(){
+    createCanvas(1000,1000);
+    fill(100,0,255);
+    ellipse(500,500,1000,1000);
+}
+let points = 0;
+let pointsInCircle = 0;
+function draw(){
+    
+}
+function addTenPoint(){
+    for(let i = 0;i< 10;i++){
+        addPoint();
+    }
+}
+function addPoint(){
+    fill(255,0,0);
+    let x;
+    let y;
+    x = random(0,1000);
+    y = random(0,1000)
+    ellipse(x,y,10,10);
+    points++;
+    console.log(dist(500,500,x,y));
+    if(dist(500,500,x,y)<500){
+        pointsInCircle++;
+    }
+    console.log("points: " + points + " InCircle: "+ pointsInCircle);
+}
+
+
+function pressButton(){
+    console.log("buttonPressed");
+}
